3abd77019a5b91f272e675d432d59db3ebdfc54c
Punam Sonawane <sonawanepunam27@gmail.com>
2020-07-10 16:11:55 -0400

    blackjack deal updated

--- static/js/home.js
+++ static/js/home.js
@@ -87,7 +87,7 @@ function rpsFrontEnd(humanImageChoice, botImageChoice, finalMessage){
 let blackjackGame={
     'you':{'scoreSpan':'#your-blackjack-result','div':'#your-box','score':0},
     'dealer':{'scoreSpan':'#dealer-blackjack-result','div':'#dealer-box', 'score':0},
-
+    'cards':['2','3','4','5','6','7','8','9','10','k','J','Q','A'],
 }
 
 const YOU= blackjackGame['you']
@@ -96,10 +96,36 @@ const DEALER= blackjackGame['dealer']
 const hitSound= new Audio('../sound/swish.m4a')
 
 document.querySelector('#blackjack-hit-btn').addEventListener('click',blackjackHit)
+document.querySelector('#blackjack-deal-btn').addEventListener('click',blackjackDeal)
 
 function blackjackHit(){
+let card=randomCard()
+console.log(card)
+showCard(card,DEALER)
+}
+
+function randomCard(){
+    let randomIndex=Math.floor(Math.random()*13)
+    return blackjackGame['cards'][randomIndex]
+}
+
+function showCard(card,activePlayer){
     let cardImage=document.createElement('img')
-    cardImage.src='../images/Q.png'
-    document.querySelector(YOU['div']).appendChild(cardImage)
+    cardImage.src=`../images/${card}.png`
+    document.querySelector(activePlayer['div']).appendChild(cardImage)
     hitSound.play()
+}
+
+//clear the cards (reset)
+function blackjackDeal(){
+    let yourImages= document.querySelector("#your-box").querySelectorAll('img')
+    let dealerImages= document.querySelector("#dealer-box").querySelectorAll('img')
+   for(i=0;i<yourImages.length;i++){
+        yourImages[i].remove();
+    } 
+
+    for(i=0;i<dealerImages.length;i++){
+        dealerImages[i].remove();
+    } 
+    console.log(yourImages)
 }
